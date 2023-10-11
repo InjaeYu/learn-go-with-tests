1334109a4bf57e9a28feba03ae9d2d5b1b7c0987
유인재 <injyu@dozn.co.kr>
2023-10-11 17:30:45 +0900

    v6: 두번째 파라미터에 언어 추가 및 이에 따른 테스트 추가

---
+++ hello-world/v6/hello.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefic = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(word, language string) string {
+	if len(word) == 0 {
+		word = "World"
+	}
+
+	prefix := englishHelloPrefic
+
+	if language == spanish {
+		prefix = spanishHelloPrefix
+	} else if language == french {
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + word
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
