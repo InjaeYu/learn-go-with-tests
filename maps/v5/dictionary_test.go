5df5d98b092da1482f44ee46d20cd10db5ec2724
유인재 <injyu@dozn.co.kr>
2023-10-19 17:24:57 +0900

    v5: Update 메서드 추가, 이에 따른 테스트 코드 추가

---
+++ maps/v5/dictionary_test.go
@@ -0,0 +1,82 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		key := "test"
+		value := "this is just a test"
+
+		err := dictionary.Add(key, value)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, key, value)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		key := "test"
+		value := "this is just a test"
+		dictionary := Dictionary{key: value}
+
+		err := dictionary.Add(key, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, key, value)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	key := "test"
+	value := "this is just a test"
+	dictionary := Dictionary{key: value}
+	newValue := "new difinition"
+
+	dictionary.Update(key, newValue)
+
+	assertDefinition(t, dictionary, key, newValue)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, key, value string) {
+	t.Helper()
+
+	got, err := dictionary.Search(key)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, value)
+}
