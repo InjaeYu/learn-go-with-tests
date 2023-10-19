6f8fd89fe7d9ada28737c8acc12f4fe70400f761
유인재 <injyu@dozn.co.kr>
2023-10-19 14:52:07 +0900

    v1: 비트코인 지갑 및 테스트 코드 추가

---
+++ pointers/v1/wallet_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
