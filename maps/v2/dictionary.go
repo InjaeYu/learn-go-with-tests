c66f0b8d2887089ad89081a435696598963b5de4
유인재 <injyu@dozn.co.kr>
2023-10-19 16:24:00 +0900

    v2: 등록되지 않은 단어 조회시 에러 반환 추가, 이에 따른 테스트 코드 추가

---
+++ maps/v2/dictionary.go
@@ -0,0 +1,16 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
