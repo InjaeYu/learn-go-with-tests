edb4ab640c3da2a2463f021d6cc7ec1a295c0742
유인재 <injyu@dozn.co.kr>
2023-10-20 17:16:33 +0900

    v2: magic number에 대해 상수로 의미 부여, 각 출력 사이에 1초 대기 추가

---
+++ mocking/v2/countdown.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
+)
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
